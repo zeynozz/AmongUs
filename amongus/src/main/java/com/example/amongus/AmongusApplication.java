994a3dac8518af4b7654d36e5294fb34af1f0321
zeynozz <146842419+zeynozz@users.noreply.github.com>
2024-04-02 01:27:39 +0200

    Project Start
    :( biktim

---
+++ amongus/src/main/java/com/example/amongus/AmongusApplication.java
@@ -0,0 +1,13 @@
+package com.example.amongus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmongusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmongusApplication.class, args);
+    }
+
+}
---
+++ amongus/src/test/java/com/example/amongus/AmongusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amongus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmongusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
